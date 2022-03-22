f7a7c2f5f09b02926be5b443bf42652d237a14cc
ParkJiHo <iks15174@daum.net>
2022-03-23 00:33:45 +0900

    1043-거짓말-unionfind

--- boj/1043.py
+++ boj/1043.py
@@ -4,29 +4,44 @@ people_know = list(map(int, input().split()))
 people_know_num = people_know[0]
 people_know = set(people_know[1:])
 
+roots = [i for i in range(0, people + 1)]
+for know in people_know:
+    roots[know] = 0
+
+
+def find_root(val):
+    if roots[val] == val:
+        return val
+    roots[val] = find_root(roots[val])
+    return roots[val]
+
+
+def union(val1, val2):
+    val1_root = find_root(val1)
+    val2_root = find_root(val2)
+
+    if val2_root < val1_root:
+        roots[val1_root] = val2_root
+    else:
+        roots[val2_root] = val1_root
+
+
 for i in range(party):
     temp = list(map(int, input().split()))
     party_list[i] = temp[1:]  # 전체 인원수를 제외하고 저장한다.
 
-
-while True:
-    prev_set_len = len(people_know)
-    for p in party_list:
-        for attendance in p:
-            if attendance in people_know:
-                people_know.update(p)
-                break
-
-    if prev_set_len == len(people_know):  # 더이상 update할 게 없을때
-        break
+    if len(party_list[i]) > 1:
+        for j in range(1, len(party_list[i])):
+            union(party_list[i][j - 1], party_list[i][j])
 
 ans = 0
 for p in party_list:
-    find = False
-    for attendance in p:
-        if attendance in people_know:
-            find = True
-            break
-    if not find:
+    is_possible = True
+    for attend in p:
+        if find_root(attend) == 0:
+            is_possible = False
+
+    if is_possible:
         ans += 1
+
 print(ans)
