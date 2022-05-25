ffcb1d409f22b4d42fddb278b87249552b698238
ParkJiHo <iks15174@daum.net>
2022-05-25 19:32:07 +0900

    16566-카드게임-다시보기

--- boj/16566.py
+++ boj/16566.py
@@ -6,6 +6,27 @@ n, m, k = map(int, input().strip().split())
 minsu = sorted(list(map(int, input().strip().split())))
 chulsu = list(map(int, input().strip().split()))
 used = [False] * (n + 1)
+parents = [i for i in range(len(minsu))]
+
+
+def find(idx):
+    global parents
+    if idx == parents[idx]:
+        return idx
+    parents[idx] = find(parents[idx])
+    return parents[idx]
+
+
+def union(idx1, idx2):
+    global m
+    if idx2 >= m:
+        return
+    p1 = find(idx1)
+    p2 = find(idx2)
+    if p1 > p2:
+        parents[p2] = p1
+    else:
+        parents[p1] = p2
 
 
 def find_card(num):
@@ -23,11 +44,6 @@ def find_card(num):
 
 for c in chulsu:
     card = find_card(c)
-    if not used[card]:
-        print(minsu[card])
-        used[card] = True
-    else:
-        while used[card]:
-            card += 1
-        print(minsu[card])
-        used[card] = True
+    real_card = find(card)
+    print(minsu[real_card])
+    union(real_card, real_card + 1)
