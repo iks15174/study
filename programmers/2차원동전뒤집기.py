ed887f1d3079e3fc62b21caa0f8924ec92644103
ParkJiHo <iks15174@daum.net>
2022-10-26 22:43:19 +0900

    프로그래머스-2차원동전뒤집기-다시보기

--- programmers/2차원동전뒤집기.py
+++ programmers/2차원동전뒤집기.py
@@ -1,41 +1,7 @@
 import math
 
-ans = math.inf 
-def update_ans(row, col, selected_row, selected_col, new_diff, one_cnt):
-    global ans
-    row_set = set()
-    col_set = set()
-    row_idx = 0
-    col_idx = 0
-    while selected_row > 0:
-        if selected_row & 1:
-            row_set.add(row_idx)
-        row_idx += 1
-        selected_row = selected_row >> 1
-    
-    while selected_col > 0:
-        if selected_col & 1:
-            col_set.add(col_idx)
-        col_idx += 1
-        selected_col = selected_col >> 1
-        
-    if len(col_set) + len(row_set) >= ans:
-        return
-    
-    for r in range(row):
-        for c in range(col):
-            if r in row_set:
-                new_diff[r][c] = (1 - new_diff[r][c])
-                one_cnt += (-1 if not new_diff[r][c] else 1)
-            if c in col_set:
-                new_diff[r][c] = (1 - new_diff[r][c])
-                one_cnt += (-1 if not new_diff[r][c] else 1)
-                
-    if one_cnt == 0:
-        ans = min(ans, len(col_set) + len(row_set))
-    
 def solution(beginning, target):
-    global ans
+    ans = math.inf
     row = len(beginning)
     col = len(beginning[0])
     diff = [[0] * col for _ in range(row)]
@@ -47,9 +13,30 @@ def solution(beginning, target):
                 one_cnt += 1
                 
     for selected_row in range(2 ** row):
-        for selected_col in range(2 ** col):
-            new_diff = [d[:] for d in diff]
-            update_ans(row, col, selected_row, selected_col, new_diff, one_cnt)
+        cnt = 0
+        new_diff = [d[:] for d in diff]
+        sr = selected_row
+        cur_row = 0
+        while sr > 0:
+            if sr & 1:
+                cnt += 1
+                for c in range(col):
+                    new_diff[cur_row][c] = 1 - new_diff[cur_row][c]
+            sr = sr >> 1
+            cur_row += 1
+        
+        for cur_c in range(col):
+            temp_sum = 0
+            for cur_r in range(row):
+                temp_sum += new_diff[cur_r][cur_c]
+            if temp_sum == 0 or temp_sum == row:
+                cnt += (0 if temp_sum == 0 else 1)
+            else:
+                cnt += math.inf
+                break
+                    
+        ans = min(ans, cnt)
+    
     if ans == math.inf:
         return -1
     else:
